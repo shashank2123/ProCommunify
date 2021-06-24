8d0a5b2d01f22f6b7dc9e7290063e78937aabf9a
shashank2123 <shashankkulal7@gmail.com>
2021-06-24 22:27:30 +0530

    Changing project structure

--- ProCommunify/settings.py
+++ ProCommunify/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'ProCommunifyApp'
+    'bootstrap3',
+    'Accounts'
 ]
 
 MIDDLEWARE = [
@@ -52,10 +53,13 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'ProCommunify.urls'
 
+
+TEMPLATES_DIR = os.path.join(BASE_DIR,'templates')
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMP_AUTH,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -76,8 +80,12 @@ WSGI_APPLICATION = 'ProCommunify.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME' : 'procommunify_db',
+        'HOST' : '127.0.0.8',
+        'PORT' : '3306',
+        'USER' : 'root',
+        'PASSWORD' : '8105'
     }
 }
 
@@ -119,7 +127,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_AUTH = os.path.join(BASE_DIR, 'static')
 
+STATICFILES_DIRS = [ STATIC_AUTH, ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
